14533686a4d3287739f3f2d077ea1d32f6996056
Markus Hausmann <Markus Hausmann@DESKTOP-JU4T20S>
2016-11-08 11:14:42 +0100

    GUI

--- DBI Praktikumsaufgabe 6/src/GUI.java
+++ DBI Praktikumsaufgabe 6/src/GUI.java
@@ -1,5 +1,29 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class GUI extends JFrame implements ActionListener
 {
+	public JFrame frame;
+	public JTextField textFeld;
+	public JLabel schrift;
+	
+	public GUI()
+	{
+		this.frame=new JFrame("Menue");
+	}
+	
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
