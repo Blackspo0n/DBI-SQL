1757b1f53ce0ce0575bec9cba312f35714363ec0
Markus Hausmann <Markus Hausmann@DESKTOP-JU4T20S>
2016-11-08 15:41:29 +0100

    Debug Version

--- DBI Praktikumsaufgabe 6/src/Datenbankanbindung.java
+++ DBI Praktikumsaufgabe 6/src/Datenbankanbindung.java
@@ -1,6 +1,10 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datenbankanbindung 
 {
@@ -8,7 +12,7 @@ public class Datenbankanbindung
 	public static Connection myConnection;
 	
 	//herstellen einer Verbindung zur Datenbank
-	public static void verbinden()
+	public static String verbinden()
 	{
 		try
 		{
@@ -17,22 +21,47 @@ public class Datenbankanbindung
 			if(myConnection.isValid(0))
 			{
 				System.out.println("Verbunden!");
+				
 			}
+			return "Verbunden!";
 		}
 		catch(SQLException e)
 		{
 			e.printStackTrace();
+			return "Fehler bei der Verbindung zur Datenbank!";
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return "Fehler bei der Verbindung zur Datenbank!";
 		}
 	}
 	/*
 	 * Abfrage an die Datenbank und verarbeiten der Informationen. 
 	 * Danach ausgeben der Informationen.
 	 */
-	public static void abfrage(int produktID)
+	public static List<Agent> abfrage(int produktID)
 	{
+		//erstellen des Abfragebefehls
+		String abfrageBefehl="SELECT o.aid, sum (o.dollars) AS u FROM orders AS o WHERE o.pid='p"+produktID+"' GROUP BY o.aid ORDER BY u DESC";
+		//erstellen der ArrayList
+		List<Agent>agents=new ArrayList<Agent>();
+		//Abfrage an Datenbank
+		try 
+		{
+			Statement statement=myConnection.createStatement();
+			ResultSet rsAbfrage=statement.executeQuery(abfrageBefehl);
+			//eintagen der Ergebnispaare in Liste
+			while(rsAbfrage.next())
+			{
+				agents.add(new Agent(rsAbfrage.getDouble("dollars"), rsAbfrage.getString("aid")));
+			}
+		}
+		catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		return agents;
 	}
 }
--- DBI Praktikumsaufgabe 6/src/GUI.java
+++ DBI Praktikumsaufgabe 6/src/GUI.java
@@ -1,28 +1,119 @@
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class GUI extends JFrame implements ActionListener
 {
 	public JFrame frame;
-	public JTextField textFeld;
+	public JTextField eingabe;
 	public JLabel schrift;
+	public JButton bestaetigen;
+	public JFrame ausgabeFenster;
+	public JTextArea ausgabe;
 	
 	public GUI()
 	{
+		/*
+		 *Erstellen und anpassen des Frames.  
+		 */
 		this.frame=new JFrame("Menue");
+		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		
+		/*
+		 * Erstellen der Eingabenansicht.
+		 */
+		
+		erstellenEingabe();
+		
+		
+		//ans Ende
+		frame.setVisible(true);
+	}
+	
+	private void erstellenEingabe()
+	{
+		eingabe=new JTextField();
+		schrift=new JLabel("Geben sie eine ProduktID ein:");
+		bestaetigen=new JButton();
+		bestaetigen.setText("bestätigen");
+		
+		//Layout zuweisen
+		GridLayout eingabeFenster=new GridLayout(3,0);
+		frame.setLayout(eingabeFenster);
+		
+		frame.add(schrift);
+		frame.add(eingabe);
+		frame.add(bestaetigen);
+		
+		eingabe.addActionListener(this);
+		bestaetigen.addActionListener(this);
+		
+		bestaetigen.setActionCommand("BESTAETIGEN");
+		
+		
+	}
+	private void erstellenAusgabe(List<Agent> list)
+	{
+		//erstelen eines neuen Frames
+		this.ausgabeFenster=new JFrame();
+		ausgabeFenster.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		ausgabeFenster.setLocationRelativeTo(null);
+		ausgabe= new JTextArea();
+		
+		
+		//einfügen in die Liste
+		ausgabe.setText("AgentID          Umsatz");
+		for (Agent row: list)
+		{
+			ausgabe.setText(ausgabe.getText()+"\n"+ row.getAgent()+"          "+row.getUmsatz());
+		}
+		
+		//hinzufügen einer Scrollbar
+			JScrollPane scrollpane = new JScrollPane(ausgabe);     
+		//ans Ende
+		ausgabeFenster.add(scrollpane);
+		ausgabeFenster.setVisible(true);
 	}
 	
 	
 	
-	
-	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent a) 
+	{
 		// TODO Auto-generated method stub
+		if(a.getActionCommand().equals("BESTAETIGEN"))
+		{
+			
+		
+			if(Datenbankanbindung.verbinden().equals("Fehler bei der Verbindung zur Datenbank!"))
+			{
+				JOptionPane.showMessageDialog(frame, "Fehler bei der Verbindung zur Datenbank!","Fehler!", JOptionPane.ERROR_MESSAGE);
+			}
+			try
+			{
+				erstellenAusgabe(Datenbankanbindung.abfrage(Integer.parseInt(eingabe.getText())));
+			}
+			catch(NumberFormatException e)
+			{
+				JOptionPane.showMessageDialog(frame, "Bitte nur Zahlen eingeben!","Fehler!", JOptionPane.ERROR_MESSAGE);
+			}
+			catch(NullPointerException e)
+			{
+				JOptionPane.showMessageDialog(frame, "Keine Einträge zur ProduktID gefunden!","Fehler!", JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}
 		
 	}
 
